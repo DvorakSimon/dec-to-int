a42c25639522422b799a530f549a5ab01e48b177
DvorakSimon <simond.dvorak@gmail.com>
2017-02-19 11:28:53 +0100

    Version 2.0 - static array
    In this version I use a static array instead of dynamic malloc.

--- main.c
+++ main.c
@@ -8,6 +8,10 @@ typedef int bool;
 #define true 1
 #define false 0
 
+// maximalni delka pole pro long.
+// jedna se o logaritmus pri zaklade 2 cisla v decimalni soustave.
+#define BIN_LENGTH 63
+
 /**
  * Zkontroluje argumenty.
  * pokud jich je moc nebo malo -> vyhodi chybovou hlasku.
@@ -16,7 +20,7 @@ bool checkArgumentCount(int argc)
 {
     if (argc - 1 == 0 || argc - 1 > 1) {
         printf("Zadejte prosim prave jeden argument.\n");
-        exit(0);
+        return false;
     } else if (argc - 1 == 1) {
         return true;
     }
@@ -33,9 +37,9 @@ bool checkParam(char *c_num[])
     char * endp;
     long num = strtol(c_num[1], &endp, 10);
 
-    if (num > INT_MAX || num < INT_MIN) {
-        printf("Zadejte prosim cislo od %d do %d.\n", INT_MIN, INT_MAX);
-        exit(0);
+    if (num > LONG_MAX || num < LONG_MIN) {
+        printf("Zadejte prosim cislo od %ld do %ld.\n", LONG_MAX, LONG_MIN);
+        return false;
     }
 
     if (!*endp != '\0') {
@@ -49,16 +53,25 @@ bool checkParam(char *c_num[])
 /**
  * Vypocita, jak bude dlouhe cislo v binarce.
  */
-int getBinLength(int decNum) {
+int getBinLength(long decNum) {
     decNum = abs(decNum);
+    if (decNum == 0) return 0;
     // novy pocet mist, ktereho bude nabyvat nove cislo v binarce.
-    return log(decNum)/log(2);
+    long result = log2l(decNum);
+    return result;
 }
 
 /**
  * Prevede cislo z decimalky do binarky.
  */
-int * convertDecToBin(int decIntToConvert) {
+char * convertDecToBin(long decIntToConvert) {
+    // deklaruji si staticky alokovane integerove pole o 63 prvku.
+    // 63 prvku, protoze round(log2(9223372036854775807)) je 63.
+    static char resultBin[BIN_LENGTH] = {0};
+
+    // promenna uchovavajici aktualni index pole "resultBin".
+    int k = 0;
+
     bool negative = false;
 
     // detekuje, ze je cislo zaporne.
@@ -66,29 +79,20 @@ int * convertDecToBin(int decIntToConvert) {
     // nakonec da na zacatek -1.
     if (decIntToConvert < 0) {
         negative = true;
-        decIntToConvert *= -1;
+        decIntToConvert = -decIntToConvert;
     }
-
-    // ziska delku cisla v binarce.
-    int binLength = getBinLength(decIntToConvert);
-
-    // deklaruji si dynamicky alokovane integerove pole o "binLength" prvku.
-    // +1 pro terminator \0
-    int * resultBin = malloc((binLength + 1) * sizeof(int));
-
-    // promenna uchovavajici aktualni index pole "resultBin".
-    int idx = binLength;
 
     // samotny prevod cisla z desitkovy do binarni soustavy.
     while (decIntToConvert != 0) {
-        resultBin[idx] = decIntToConvert % 2;
+        resultBin[k++] = decIntToConvert % 2;
         decIntToConvert /= 2;
-        idx--;
     }
+
+    k--;
 
     // pokud je cislo zaporne, nastavi prvni cislo na -1.
     if (negative == 1) {
-        resultBin[0] = -1;
+        resultBin[k] = -1;
     }
 
     return resultBin;
@@ -97,11 +101,16 @@ int * convertDecToBin(int decIntToConvert) {
 /**
  * Vypise vyslednou frazi.
  */
-void printBinNumber(int binLength, int decNum, int * binNum) {
-    printf("Cislo v dekadicke soustave %d je ", decNum);
+void printBinNumber(long decNum, char * binNum) {
+    printf("Cislo v dekadicke soustave %ld je ", decNum);
 
-    int f = 0;
-    for(;f <= binLength; f++)
+    int f = BIN_LENGTH;
+
+    while (binNum[f] == 0 && f > 0) {
+        f--;
+    }
+
+    for(; f >= 0; f--)
         printf("%d", binNum[f]);
     printf(" v binarni soustave.\n");
 }
@@ -109,27 +118,19 @@ void printBinNumber(int binLength, int decNum, int * binNum) {
 int main(int argc, char *argv[])
 {
     bool checkArgCount = checkArgumentCount(argc);
-    bool checkParamInt = checkParam(argv);
 
-    if (checkArgCount == 1 && checkParamInt == 1) {
-        // preved char zadany do argumentu na integer.
-        int decIntToConvert = atoi(argv[1]);
+    if (checkArgCount == 1) {
 
-        // zkouska, ze uzivatel nezadal nula (nebo minus nula).
-        // aby nedoslo k deklaraci pole o velikosti 0 -> nastala by chyba.
-        // -0 = 0 (ano cecko to vi).
-        if (decIntToConvert == 0) {
-            int zero_array[1] = {0};
-            printBinNumber(0, decIntToConvert, zero_array);
-            return 0;
+        bool checkParamInt = checkParam(argv);
+
+        if (checkParamInt == 1) {
+            // preved char zadany do argumentu na integer.
+            long decIntToConvert = atol(argv[1]);
+
+            char * result = convertDecToBin(decIntToConvert);
+
+            printBinNumber(decIntToConvert, result);
         }
-
-        int * result = convertDecToBin(decIntToConvert);
-
-        int binLength = getBinLength(decIntToConvert);
-        printBinNumber(binLength, decIntToConvert, result);
-
-        free(result);
     }
 
     return 0;
